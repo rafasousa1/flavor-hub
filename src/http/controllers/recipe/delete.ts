f7e55cd54e380020e8df7ddc92c796a832e2340f
rafasousa1 <rafasousasilva01@gmail.com>
2025-10-21 14:39:27 -0300

    feat: recipe controller

---
+++ src/http/controllers/recipe/delete.ts
@@ -0,0 +1,28 @@
+import { ResourceNotFound } from '@/use-case/errors/resource-not-found'
+import { makeDeleteRecipeUseCase } from '@/use-case/factories/make-delete-recipe'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function deleteRecipe(req: FastifyRequest, reply: FastifyReply) {
+    const deleteRecipeBodySchema = z.object({
+        recipeId: z.string().uuid()
+    })
+
+    const {recipeId } = deleteRecipeBodySchema.parse(req.body)
+    
+    try { 
+    const useCase = makeDeleteRecipeUseCase()
+
+    await useCase.execute({
+        recipeId
+    })
+    } catch (err) {
+        if (err instanceof ResourceNotFound) {
+            return reply.status(404).send({ message: err.message })
+        }
+
+        throw err
+    }
+
+    return reply.status(204).send()
+}
--- src/http/controllers/recipe/routes.ts
+++ src/http/controllers/recipe/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { verifyJWT } from '@/http/hooks/verify-jwt'
+import { deleteRecipe } from './delete'
 
 export default function recipeRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
     
     app.post('/recipes', create)
+    app.delete('/recipes/:recipeId', deleteRecipe)
 }
